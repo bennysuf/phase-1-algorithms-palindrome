a770bd9cabd0db99727736d352528b2c4a54af85
Benny Sufrin <bennysuf@gmail.com>
2022-09-11 14:24:38 -0400

    commit tree 6e615019badfe8fb91c4b969f897f9a67448587b

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const arr = Array.from(word); //separates word into separate letters
+  arr.reverse() // reverses order or letters
+  const convert = arr.join(""); //removes commas
+  
+  if (convert === word) {
+    return true 
+  } else {
+    return false
+  }
+  //   return "Input: " + word + " Output: " + true
+  // } else { return "Input: " + word + " Output: " + false;
+  // console.log(String(word))
+//  }
 }
 
 /* 
   Add your pseudocode here
+  turn into an array
+  try to flip array order into new array
+  check if new array is equal to old array
+  return boolean result 
 */
 
 /*
   Add written explanation of your solution here
+  function takes argument and converts it into an array of separate letters.
+  reverses the order of the letters and removes commas to make it one word again.
+  checks if original argument matches the reversed converted word.
 */
 
 // You can run `node index.js` to view these console logs
+
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
